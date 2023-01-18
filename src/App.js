c6e1e65dcd05cf9471f33e0f04aac014ca03fef8
Xiaocheng Liu <xiaochengliu@Xiaochengs-MacBook-Pro.local>
2023-01-18 11:23:54 -0500

    Sign In Component created and set up Route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './routes/home/home.conponent'
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
+const Shop = () => {
+  return (
+    <div>
+      <h1>Shop Page</h1>
+    </div>
+  )
+}
 
 const App = () => {
   return (
@@ -11,6 +19,8 @@ const App = () => {
         <Route index element={ <Home /> }/> 
         {/* index is an attribute, and is actually index={true},
         it tells the route, when you match just the "/", then this route should be Home component*/}
+        <Route path='shop' element={<Shop />} />
+        <Route path='signIn' element={<SignIn />} />
       </Route>
     </Routes>
   )
