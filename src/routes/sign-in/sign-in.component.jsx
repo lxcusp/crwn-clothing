31b0fb188d5e8685f565a2ec64529c87c1d44ebc
Xiaocheng Liu <xiaochengliu@Xiaochengs-MacBook-Pro.local>
2023-01-19 13:24:25 -0500

    delect the previous and restore back to SignIn method retain

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,16 +1,6 @@
-import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from "../../utils/firebase/firebase.utils"
-import { useEffect } from 'react'
-import { getRedirectResult } from "firebase/auth"
+import { signInWithGooglePopup, createUserDocumentFromAuth, } from "../../utils/firebase/firebase.utils"
 
 const SignIn = () => {
-    //run this useEffect once when the component first-time mounted
-    useEffect(async () => {
-        const response = await getRedirectResult(auth);
-        // console.log(response);
-        if(response) {
-            const userDocRef = await createUserDocumentFromAuth(response.user)
-        }
-    }, [])
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
@@ -22,7 +12,6 @@ const SignIn = () => {
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
         </div>
     )
 }
