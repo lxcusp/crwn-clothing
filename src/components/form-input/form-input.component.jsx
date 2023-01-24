a86a78475299fe2110e7fca51cf6b9080d2bfb4b
Xiaocheng Liu <xiaochengliu@Xiaochengs-MacBook-Pro.local>
2023-01-24 15:01:47 -0500

    restore the code to previous version

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,15 +1,15 @@
 import './form-input.styles.scss'
 
-const FormInput = ({ label, inputOptions }) => {
+const FormInput = ({ label, ...otherProps }) => {
     return (
         <div className="group">
-            <input className="form-input" {...inputOptions} />
+            <input className="form-input" {...otherProps} />
             {/* if this input put below the label, the subsequent selector is not gonna work,
             because the selector is looking for general sibling component with the label after the input,
             but we want it when cursor locate the field, the label shrink*/}
 
             {label && //if label exists, then render the label below
-                <label className={`${inputOptions.value.length ? 'shrink' : ''} form-input-label`}>{label}</label>
+                <label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>{label}</label>
                 //when user tries to type something in(the value > 0), the label shrink
             }
         </div>
